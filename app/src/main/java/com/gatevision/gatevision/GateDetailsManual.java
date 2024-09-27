0f0c71b3c1ac6130f178c43ac9138b6d91e23c85
PavanKalisetti <itspavan09@gmail.com>
2024-09-27 22:24:25 +0530

    UI/UX completed

--- app/src/main/java/com/gatevision/gatevision/GateDetailsManual.java
+++ app/src/main/java/com/gatevision/gatevision/GateDetailsManual.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,13 +19,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+
+import java.io.IOException;
+
+
 public class GateDetailsManual extends AppCompatActivity {
 
-    CardView GateDetailsSlidingGateCardview, GateDetailsSwingGateCardview, GateDetailsOthersCardview;
-    Boolean[] typeOfGate = {false, false, false};
-    TextView GateDetailsSlidingText, GateDetailsSwingGateText, GateDetailsOthersText;
+    CardView GateDetailsSlidingGateCardview, GateDetailsSwingGateCardview, CablingProvisions, MaterialStorage;
+    Boolean[] typeOfGate = {false, false};
+    Boolean[] CablingAndMaterial = {false, false};
+    TextView GateDetailsSlidingText, GateDetailsSwingGateText, CablingProvisionsText, MaterialStorageText;
 
     Button GateDetailsSubmitButton;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int STORAGE_PERMISSION_CODE = 101;
+
+    private Button uploadImageButton;
+    private ImageView imagePreview;
+
+    private CardView uploadImgCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +63,19 @@ public class GateDetailsManual extends AppCompatActivity {
 
         GateDetailsSlidingGateCardview = findViewById(R.id.GateDetailsSlidingGateCardview);
         GateDetailsSwingGateCardview = findViewById(R.id.GateDetailsSwingGateCardview);
-        GateDetailsOthersCardview = findViewById(R.id.GateDetailsOthersCardview);
+
         GateDetailsSlidingText = findViewById(R.id.GateDetailsSlidingText);
         GateDetailsSwingGateText = findViewById(R.id.GateDetailsSwingGateText);
-        GateDetailsOthersText = findViewById(R.id.GateDetailsOthersText);
+
         GateDetailsSubmitButton = findViewById(R.id.GateDetailsSubmitButton);
+
+        uploadImgCardView = findViewById(R.id.uploadImgCardView);
+
+
+        CablingProvisions = findViewById(R.id.CablingProvisions);
+        MaterialStorage = findViewById(R.id.MaterialStorage);
+        CablingProvisionsText = findViewById(R.id.CablingProvisionsText);
+        MaterialStorageText = findViewById(R.id.MaterialStorageText);
 
 
         GateDetailsSlidingGateCardview.setOnClickListener(new View.OnClickListener() {
@@ -52,10 +84,9 @@ public class GateDetailsManual extends AppCompatActivity {
                 if(!typeOfGate[0]){
                     GateDetailsSlidingGateCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#2277F6")));
                     GateDetailsSwingGateCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ECF0F1")));
-                    GateDetailsOthersCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ECF0F1")));
+
                     GateDetailsSlidingText.setTextColor(getResources().getColor(R.color.white));
                     GateDetailsSwingGateText.setTextColor(getResources().getColor(R.color.black));
-                    GateDetailsOthersText.setTextColor(getResources().getColor(R.color.black));
                     typeOfGate[0] = true;
                 }else{
                     GateDetailsSlidingGateCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ECF0F1")));
@@ -74,10 +105,8 @@ public class GateDetailsManual extends AppCompatActivity {
                 if(!typeOfGate[1]){
                     GateDetailsSwingGateCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#2277F6")));
                     GateDetailsSlidingGateCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ECF0F1")));
-                    GateDetailsOthersCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ECF0F1")));
                     GateDetailsSwingGateText.setTextColor(getResources().getColor(R.color.white));
                     GateDetailsSlidingText.setTextColor(getResources().getColor(R.color.black));
-                    GateDetailsOthersText.setTextColor(getResources().getColor(R.color.black));
                     typeOfGate[1] = true;
                 }else{
                     GateDetailsSwingGateCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ECF0F1")));
@@ -89,20 +118,32 @@ public class GateDetailsManual extends AppCompatActivity {
             }
         });
 
-        GateDetailsOthersCardview.setOnClickListener(new View.OnClickListener() {
+        CablingProvisions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!typeOfGate[2]){
-                    GateDetailsOthersCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#2277F6")));
-                    GateDetailsSlidingGateCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ECF0F1")));
-                    GateDetailsSwingGateCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ECF0F1")));
-                    GateDetailsOthersText.setTextColor(getResources().getColor(R.color.white));
-                    GateDetailsSlidingText.setTextColor(getResources().getColor(R.color.black));
-                    GateDetailsSwingGateText.setTextColor(getResources().getColor(R.color.black));
-                    typeOfGate[2] = true;
+                if(!CablingAndMaterial[0]){
+                    CablingProvisions.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#2277F6")));
+                    CablingProvisionsText.setTextColor(getResources().getColor(R.color.white));
+                    CablingAndMaterial[0] = true;
                 }else{
-                    GateDetailsOthersCardview.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ECF0F1")));
-                    typeOfGate[2] = false;
+                    CablingProvisions.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ECF0F1")));
+                    CablingProvisionsText.setTextColor(getResources().getColor(R.color.black));
+                    CablingAndMaterial[0] = false;
+                }
+            }
+        });
+
+        MaterialStorage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!CablingAndMaterial[1]){
+                    MaterialStorage.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#2277F6")));
+                    MaterialStorageText.setTextColor(getResources().getColor(R.color.white));
+                    CablingAndMaterial[1] = true;
+                }else{
+                    MaterialStorage.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ECF0F1")));
+                    MaterialStorageText.setTextColor(getResources().getColor(R.color.black));
+                    CablingAndMaterial[1] = false;
                 }
             }
         });
@@ -115,5 +156,79 @@ public class GateDetailsManual extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        uploadImageButton = findViewById(R.id.uploadImageButton);
+        imagePreview = findViewById(R.id.imagePreview);
+
+        // Set a click listener to open the gallery
+        uploadImageButton.setOnClickListener(v -> {
+            if (checkPermission()) {
+                openImagePicker();
+            } else {
+                requestStoragePermission();
+            }
+        });
+
+
     }
+
+
+    // Method to open image picker
+    private void openImagePicker() {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+
+    }
+
+    // Check for storage permission
+    private boolean checkPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    // Request storage permission
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                STORAGE_PERMISSION_CODE);
+    }
+
+    // Handle permission result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openImagePicker();
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Handle the image selected from gallery
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+            try {
+                // Convert image URI to Bitmap and display it in the ImageView
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                imagePreview.setImageBitmap(bitmap);
+                uploadImgCardView.setVisibility(View.GONE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/gatevision/gatevision/Output.java
+++ app/src/main/java/com/gatevision/gatevision/Output.java
@@ -28,36 +28,12 @@ public class Output extends AppCompatActivity {
         });
 
 
-        TextView OutputDetailsOfGate = findViewById(R.id.OutputDetailsOfGate); // Make sure to set the correct ID
 
-        SpannableStringBuilder builder = new SpannableStringBuilder();
 
-        // Add each bullet point
-        addBulletPoint(builder, "Durability: Gates are designed to withstand various weather conditions, ensuring long-lasting performance and minimal maintenance.");
-        addBulletPoint(builder, "Security: Provides enhanced security for properties, acting as a barrier against unauthorized access.");
-        addBulletPoint(builder, "Aesthetic Appeal: Available in various designs, colors, and materials, enhancing the overall look of your property.");
-        addBulletPoint(builder, "Customization: Can be tailored to fit specific sizes and styles, ensuring they meet your unique needs.");
-        addBulletPoint(builder, "Ease of Use: Equipped with user-friendly mechanisms (manual or automatic) for convenient operation.");
-        addBulletPoint(builder, "Versatility: Suitable for residential, commercial, and industrial applications, serving multiple purposes.");
-        addBulletPoint(builder, "Material Options: Common materials include wood, metal, and vinyl, each offering distinct advantages in terms of style and maintenance.");
-        addBulletPoint(builder, "Privacy: Solid gates can provide an added level of privacy for homes and yards.");
-        addBulletPoint(builder, "Environmentally Friendly: Options available in sustainable materials and designs that minimize environmental impact.");
 
-        OutputDetailsOfGate.setText(builder);
 
     }
 
 
-    private void addBulletPoint(SpannableStringBuilder builder, String text) {
-        int start = builder.length();
-        builder.append(text + "\n"); // Append text with a newline
-        int end = builder.length();
 
-        // Add bullet span
-        builder.setSpan(new BulletSpan(10), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-        // Make the title part bold
-        int titleEnd = start + text.indexOf(":") + 1; // Find the position of the colon
-        builder.setSpan(new StyleSpan(Typeface.BOLD), start, titleEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-    }
 }
